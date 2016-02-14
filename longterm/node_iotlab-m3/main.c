4126ebf6c42c2153991c7b7424bdcb316829f67e
Cenk Gündoğan <cnkgndgn@gmail.com>
2016-02-14 11:13:01 +0100

    iotlab-m3 app: added coap endpoint to handle the red led

--- longterm/node_iotlab-m3/main.c
+++ longterm/node_iotlab-m3/main.c
@@ -38,12 +38,14 @@
 #include "periph/gpio.h"
 #include "isl29020.h"
 #include "lps331ap.h"
+#include "periph/gpio.h"
 
 #define UPDATE_INTERVAL     (1000 * 1000U)
 #define MSG_UPDATE_EVENT    (0x3338)
 
 #define Q_SZ                (4)
 #define PRIO                (THREAD_PRIORITY_MAIN - 1)
+#define COAP_SERVER_PORT    (5683)
 #define SPORT               (1234)
 #define UDP_PORT            (5683)
 
@@ -51,12 +53,15 @@
 #define LIGHT_RANGE         ISL29020_RANGE_16K
 #define TP_RATE             LPS331AP_RATE_7HZ
 
-static msg_t _beac_msg_q[Q_SZ], _main_msg_q[Q_SZ];
-static char beac_stack[THREAD_STACKSIZE_DEFAULT];
+static msg_t _beac_msg_q[Q_SZ], _main_msg_q[Q_SZ], _coap_msg_q[Q_SZ];
+static char coap_stack[THREAD_STACKSIZE_DEFAULT], beac_stack[THREAD_STACKSIZE_DEFAULT];
 
 static isl29020_t light_dev;
 static lps331ap_t tp_dev;
 
+static uint8_t udp_buf[512];
+static uint8_t scratch_raw[1024];      /* microcoap scratch buffer */
+static coap_rw_buffer_t scratch_buf = { scratch_raw, sizeof(scratch_raw) };
 static ipv6_addr_t dst_addr;
 
 /* buffer for composing SemML messages in */
@@ -70,6 +75,74 @@ static const coap_header_t req_hdr = {
         .code    = COAP_METHOD_POST,
         .mid     = {5, 57}            // is equivalent to 1337 when converted to uint16_t
 };
+
+static const coap_endpoint_path_t path_led = { 1, { "led" } };
+
+static int handle_post_led(coap_rw_buffer_t *scratch,
+                           const coap_packet_t *inpkt, coap_packet_t *outpkt,
+                           uint8_t id_hi, uint8_t id_lo)
+{
+    coap_responsecode_t resp = COAP_RSPCODE_CHANGED;
+    uint8_t val = inpkt->payload.p[0];
+
+    if ((inpkt->payload.len == 1) && ((val == '1') || (val == '0'))) {
+        gpio_write(LED_RED_GPIO, (val - '1'));
+    }
+    else {
+        resp = COAP_RSPCODE_NOT_ACCEPTABLE;
+    }
+
+    return coap_make_response(scratch, outpkt, NULL, 0,
+                              id_hi, id_lo, &inpkt->token, resp,
+                              COAP_CONTENTTYPE_TEXT_PLAIN, false);
+}
+
+const coap_endpoint_t endpoints[] =
+{
+    { COAP_METHOD_POST,  handle_post_led, &path_led, "ct=0" },
+    /* marks the end of the endpoints array: */
+    { (coap_method_t)0, NULL, NULL, NULL }
+};
+
+void *microcoap_server(void *arg)
+{
+    (void) arg;
+    msg_init_queue(_coap_msg_q, Q_SZ);
+
+    uint8_t laddr[16] = { 0 };
+    uint8_t raddr[16] = { 0 };
+    size_t raddr_len;
+    uint16_t rport;
+    conn_udp_t conn;
+    int rc = conn_udp_create(&conn, laddr, sizeof(laddr), AF_INET6, COAP_SERVER_PORT);
+
+    while (1) {
+        if ((rc = conn_udp_recvfrom(&conn, (char *)udp_buf, sizeof(udp_buf),
+                                    raddr, &raddr_len, &rport)) < 0) {
+            continue;
+        }
+
+        coap_packet_t pkt;
+        /* parse UDP packet to CoAP */
+        if (0 == (rc = coap_parse(&pkt, udp_buf, rc))) {
+            coap_packet_t rsppkt;
+
+            /* handle CoAP request */
+            coap_handle_req(&scratch_buf, &pkt, &rsppkt, false, false);
+
+            /* build reply */
+            size_t rsplen = sizeof(udp_buf);
+            if ((rc = coap_build(udp_buf, &rsplen, &rsppkt)) == 0) {
+                /* send reply via UDP */
+                rc = conn_udp_sendto(udp_buf, rsplen, NULL, 0, raddr, raddr_len,
+                                     AF_INET6, COAP_SERVER_PORT, rport);
+            }
+        }
+    }
+
+    /* never reached */
+    return NULL;
+}
 
 void send_coap_post(uint8_t *data, size_t len)
 {
@@ -102,6 +175,7 @@ void send_coap_post(uint8_t *data, size_t len)
 
 static void send_update(size_t pos, char *buf)
 {
+    char led = (gpio_read(LED_RED_GPIO)) ? '0' : '1';
     int lux, pres, pres_abs, temp, temp_abs;
 
     lux = isl29020_read(&light_dev);
@@ -113,6 +187,8 @@ static void send_update(size_t pos, char *buf)
     temp_abs = temp / 1000;
     temp -= temp_abs * 1000;
 
+    pos += sprintf(&buf[pos], "{\"n\":\"a:led\", \"u\":\"bool\", \"v\":\"%c\"},",
+                   led);
     pos += sprintf(&buf[pos], "{\"n\":\"s:light\", \"u\":\"lux\", \"v\":\"%d\"},",
                    lux);
     pos += sprintf(&buf[pos], "{\"n\":\"s:pressure\", \"u\":\"bar\", \"v\":\"%2i.%03i\"},",
@@ -173,8 +249,6 @@ int main(void)
     ipv6_addr_from_str(&dst_addr, "beef::1");
     // ipv6_addr_from_str(&dst_addr, "fd38:3734:ad48:0:211d:50ce:a189:7cc4");
 
-
-
     /* initialize senml payload */
     gnrc_netapi_get(ifs[0], NETOPT_IPV6_IID, 0, &iid, sizeof(eui64_t));
 
@@ -188,6 +262,12 @@ int main(void)
     isl29020_init(&light_dev, ISL29020_I2C, ISL29020_ADDR, LIGHT_RANGE, LIGHT_MODE);
     lps331ap_init(&tp_dev, LPS331AP_I2C, LPS331AP_ADDR, TP_RATE);
 
+    LED_GREEN_OFF;
+    LED_ORANGE_OFF;
+    LED_RED_OFF;
+
+    thread_create(coap_stack, sizeof(coap_stack), PRIO - 1, THREAD_CREATE_STACKTEST, microcoap_server,
+                  NULL, "coap");
     thread_create(beac_stack, sizeof(beac_stack), PRIO, THREAD_CREATE_STACKTEST, beaconing,
                   NULL, "beaconing");
 
